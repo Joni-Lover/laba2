465281d682a41f9158a0e45cad83696ebbf119ae
Ivan Polonevich <loverjoni@gmail.com>
2015-12-17 12:04:46 +0300

    add additional info from file

--- find.cpp
+++ find.cpp
@@ -4,44 +4,45 @@
 #include <dirent.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define NAME_MAX 4096
+#define MAX 4096 //max namelenth
 
 int findFile(const char *filename, const char *dirname, int num_fd)
 {
   DIR *dir_for_find;
   struct stat info;
   struct dirent *entry;
-  char longest_name[NAME_MAX];
+  char *date;
+  char full_path[MAX];
 
   if (!(dir_for_find = opendir(dirname)))
-      _exit;
+    exit(EXIT_FAILURE);
 
   // Main cycle for find needet file in dir and subdir
-  while ( (entry = readdir(dir_for_find)) != NULL)
-  {
+  while ( (entry = readdir(dir_for_find)) != NULL) {
     // Don't descend up the tree or include the current directory
     if ( !strcmp(entry->d_name, ".") ) continue;
     if ( !strcmp(entry->d_name, "..") ) continue;
       num_fd++;
-      if ( entry->d_type == DT_DIR )
-      {
+      if ( entry->d_type == DT_DIR ) {
         // Prepend the current directory and recurse
-        strncpy(longest_name, dirname, NAME_MAX);
-        strncat(longest_name, "/", NAME_MAX);
-        strncat(longest_name, entry->d_name, NAME_MAX);
-        findFile(filename, longest_name, num_fd);
+        strncpy(full_path, dirname, MAX);
+        strncat(full_path, "/", MAX);
+        strncat(full_path, entry->d_name, MAX);
+        findFile(filename, full_path, num_fd);
       }
-      else if ( strcmp(entry->d_name, filename)== 0 )
-      {
-        // Prepend the current directory and recurse
-        strncpy(longest_name, dirname, NAME_MAX);
-        strncat(longest_name, "/", NAME_MAX);
-        strncat(longest_name, entry->d_name, NAME_MAX);
-        stat(longest_name, &info);
-        printf ("File is found: %s\n", longest_name);
-//        printf ("Size in bytes: %s\n Time created: %n\n Permissions: %s\n Number of index: %s\n\n", info.st_size, info.st_ctime, info.st_mode, info.st_ino);
-        printf ("Size in bytes: %li\n", info.st_size);
+      else if ( strcmp(entry->d_name, filename)== 0 ) {
+        // Prepend the current full path
+        strncpy(full_path, dirname, MAX);
+        strncat(full_path, "/", MAX);
+        strncat(full_path, entry->d_name, MAX);
+        stat(full_path, &info);
+
+        printf ("File is found: %s\n", full_path);
+        date = asctime(localtime(&info.st_ctime));
+        printf (" Size in bytes: %ld\n Time created: %s Permissions: %o\n Number of index: %lu\n\n", info.st_size, date, info.st_mode, info.st_ino);
       }
    }
   closedir(dir_for_find);
@@ -51,18 +52,21 @@ int findFile(const char *filename, const char *dirname, int num_fd)
 int main(int argc, char **argv)
 {
   struct stat info;
-  char filename[NAME_MAX];
-  char dirname[NAME_MAX];
+  char filename[MAX];
+  char dirname[MAX];
   int num;
 
-  if ( argc < 3 ) {
+  if ( argc != 3 ) {
     // Check number arguments
     printf ("Error count arguments\n");
+    fprintf(stderr, "Example usage: %s /root id_rsa.pub\n\n", argv[0]);
+    exit(EXIT_FAILURE);
   }
   else {
     if( stat( argv[1], &info ) != 0) {
       // Check access to dir
-      printf ( "Cannot access %s\n", argv[1] );
+      fprintf (stderr, "Cannot access %s\n", argv[1] );
+      exit(EXIT_FAILURE);
     }
     else if( info.st_mode & S_IFDIR ) {
       // Check is a dir and assigne variables
@@ -70,9 +74,12 @@ int main(int argc, char **argv)
       strcpy(filename, argv[2]);
     }
     else {
-      printf ( "%s is no directory\n", argv[1] );
+      fprintf (stderr, "%s is no directory\n", argv[1] );
+      exit(EXIT_FAILURE);
     }
     num=findFile(filename, dirname, 0);
     printf ("Count of checked files: %i\n", num);
+
+    exit(EXIT_SUCCESS);
   }
 };
